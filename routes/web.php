680492f1d263a8f37bf8522965a2c9da6648daf5
Reeptto <0204ariaprianto@gmail.com>
2025-09-06 10:52:45 +0700

    latihan-laravel

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return "Hello world!!";
+});
+
+Route::get('/nama', function () {
+    return 'Nama saya Ari aprianto';
+});
